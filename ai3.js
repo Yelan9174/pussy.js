918430b8208fb57a76efc6b0392b166207356613
Yelan9174 <153437226+Yelan9174@users.noreply.github.com>
2023-12-22 18:31:05 +0600

    Update ai3.js

--- ai3.js
+++ ai3.js
@@ -1,65 +1,31 @@
-const { OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-  apiKey: "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", //add your key here
-});
-const openai = new OpenAIApi(configuration);
+const axios = require('axios');
 
 module.exports = {
   config: {
     name: "ai3",
-    version: "1.1",
-    author: "kivv",
+    aliases: ["openai"],
+    version: "1.0",
+    author: "",
     countDown: 5,
     role: 0,
-    shortDescription: {
-      vi: "",
-      en: ""
-    },
-    longDescription: {
-      vi: "",
-      en: "AI powered by OpenAI"
-    },
+    shortDescription: "Ask anything",
+    longDescription: "Ask anything",
     category: "ai",
     guide: {
-      en: "{p}{n} <<question>>",
-    }
+      en: "{pn} ask"
+    },
   },
 
-  onStart: async function ({ event, message, getLang, usersData, api, args}) {
-    const { senderID } = event;
-    const userData = await usersData.get(senderID);
-    const userMoney = await usersData.get(event.senderID, "money");
-    if (userMoney < 0) return api.sendMessage("You don't have enough balance to use this command. This command costs $0.\n Check your bot balance by typing *bal.\n To earn balance, you can play games or claim daily rewards by typing *daily.\n Available games to play are *quiz and *play. ", event.threadID, event.messageID);
-    else {
-      usersData.set(event.senderID, {
-        money: userData.money - 0,
-        data: userData.data
-      }); 
-      if (args.length === 0) {
-        message.reply(`Please ask a question or add some text.`);
-        return;
-      }
-      if (args.length === 1) {
-        message.reply(`Hi there! How can I help you?`);
-        return;
-      }
-
-      try {
-        let completion = await openai.createCompletion({
-          model: "text-davinci-003",
-          prompt: args.join(" "),
-          temperature: 0.7,
-          max_tokens: 974,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0,
-        });
-        message.reply(completion.data.choices[0].text);
-      } catch (error) {
-        console.log(error);
-        message.reply(`Something went wrong.`);
-      }
+  onStart: async function ({ message, args, api, event }) {
+    const { messageID, threadID } = event;
+    const response = args.join(" ");
+    if (!response) return api.sendMessage("How may I help you?", threadID, messageID);
+    try {
+      const res = await axios.get(`https://sim.ainz-project.repl.co/others/gpt?prompt=${response}`);
+      const respond = res.data.result;
+      api.sendMessage(respond, threadID, messageID);
+    } catch (error) {
+      api.sendMessage("An error occurred while making the API request.", threadID, messageID);
     }
   }
 };
